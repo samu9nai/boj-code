132be3a0148c884c1d755cc5eb32ff4b6411998e
Mingyu Joung <samu9nai@gmail.com>
2026-03-11 16:19:13 +0900

    각 폴더에서 문제 실행 가능

---
+++ 1_Bronze/1000/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println(a+b);
+    }
+}
